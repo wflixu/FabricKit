9d4f3d012e2acecd51065c62734f2f374ed8cd1a
wflixu <wflixu@88.com>
2025-04-12 22:58:59 +0800

    Initial Commit

---
+++ Fabric/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fabric
+//
+//  Created by 李旭 on 2025/4/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fabric/FabricApp.swift
@@ -0,0 +1,17 @@
+//
+//  FabricApp.swift
+//  Fabric
+//
+//  Created by 李旭 on 2025/4/12.
+//
+
+import SwiftUI
+
+@main
+struct FabricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
